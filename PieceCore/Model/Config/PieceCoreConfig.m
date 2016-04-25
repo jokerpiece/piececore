e5bbf86f1c801f89f30ce880666403e4889e774e
Toraya <rina.ohnuma@gmail.com>
2016-04-25 14:03:56 +0900

    url訂正

--- PieceCore/Model/Config/PieceCoreConfig.m
+++ PieceCore/Model/Config/PieceCoreConfig.m
@@ -35,13 +35,10 @@ static NSString *_paypal_bncode = @"";
 static NSString *_PayPalEnvironmentProductionClientId = @"YOUR_CLIENT_ID_FOR_PRODUCTION";
 static NSString *_PayPalEnvironmentSandboxClientId = @"YOUR_CLIENT_ID_FOR_SANDBOX";
 
-NSString * const  ServerUrl= @"http://192.168.77.200/piece_dev/manager/html/xml/";
-//NSString * const  ServerUrl= @"https://jokapi.jp/manager/html/xml/";
-
+NSString * const  ServerUrl= @"https://jokapi.jp/manager/html/xml/";
 
 NSString * const  OsType= @"1";
-//NSString * const  SendTokenUrl= @"https://jokapi.jp/manager/html/xml/device_token/";
-NSString * const  SendTokenUrl= @"http://192.168.77.200/piece_dev/manager/html/xml/device_token/";
+NSString * const  SendTokenUrl= @"https://jokapi.jp/manager/html/xml/device_token/";
 NSString * const  SendIdCategory= @"category/";
 NSString * const  SendIdItem= @"item/index.php?Action=itemList";
 NSString * const  SendIdItemCoupon= @"item/";
